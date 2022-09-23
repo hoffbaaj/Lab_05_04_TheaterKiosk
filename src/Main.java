73423e433f072ab1909d1ac73f7a4ae72ac21df7
hoffbaaj <hoffbaaj@mail.uc.edu>
2022-09-22 21:22:15 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int age = 21;
+
+        if (age >= 21)
+            System.out.println("Here is your wrist band");
+    }
+}
